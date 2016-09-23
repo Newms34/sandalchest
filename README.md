70acd48772c9a8a6135e0073c67659dcc0b0bc45
newms34 <newms3450@gmail.com>
2016-09-23 18:05:41 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,75 @@
+#Sandal Chest
+##Thematically-Appropriate Bootbox
+
+##Contents:
+
+ - [What it is](#what-is-it)
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Examples](#examples)
+ - [Credits](#credits)
+ - [License](#license)
+
+##What it is
+If you're a web developer, you're probably familiar with the ubiquitous [Bootstrap](http://getbootstrap.com/). If you're not... where have you been? And if you're a dude like me that likes to churn out apps quickly and not spend too much time on styling, you're probably used to the amazing [Bootbox](http://bootboxjs.com/) library (which leverages bootstrap styling to make prettier alerts, confirms, etc.). 
+
+That's great, but what if we don't want boostrappy-looking dialogs? What if we want something a little more Ye Olde looking for the epic browser game we're writing? Enter Sandal Chest, which is like your Grandma's version of bootbox.
+
+##Installation
+Install Sandal Chest is pretty simple. It's got three methods, and all you need to do is include the css (because this uses custom styling!) and js files to use them. So install with:
+
+```bower install sandal-chest```
+
+Then slap that js file into your html:
+
+```<script src="./sandal-chest/sandal-chest.js"></script>```
+
+And finally squeeze that css in there too:
+
+```<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Bad+Script">```
+
+You'll also need a few external files, including jQuery and Bootstrap. But know you, you probably already had those in your project anyway.
+
+##Usage
+Once you've included the files above in your HTML, call them at any point in your JS by just running:
+
+```sandalchest.alert(args)```
+
+or
+
+```sandalchest.confirm(args)```
+
+or
+
+```sandalchest.prompt(args)```
+
+You'll need to give Sandal Chest at least one argument if you want it to actually do anything. Note that these are all optional.
+ - A title for your dialog. That's 'Alerts are cool!' in the screenie below.
+ - Some text for your dialog. That's 'All the cool kids are using alerts!' in the screenie below.
+ - A callback function. If you use ```sandalchest.prompt()``` or ```sandalchest.confirm()```, your input will be passed to this function. 
+ - An options object (see below);
+
+These can be included in any order. However, there are a few specifics:
+
+ 1. The first argument of type 'string' will be treated as the title, and the second will be treated as the main text.
+ 2. If you supply more than the maximum number of arguments of a type (so, more than 2 strings, or more than 1 function or object), Sandal Chest will throw an error.
+ 3. Again, these are all optional. 
+  - Not supplying the title will default it to 'Untitled'. 
+  - Not supplying the text will default it to 'No content!'.
+  - Not supplying the options object will run the function with default options (speed of 1s, rotation offset of 2 degrees).
+
+The options object (currently!) has two options: Speed and rotation. The defaults for these are 1000ms and 2 degrees, but they can be changed by passing something like ```{speed:3000,rotation:20}``` to the function to, for example, run a dialog with a 3-second intro duration, and a rotation of 20 degrees.
+
+Here's a nice screenshot for you:
+![Ye Olde User Interface Example](./tests/screenie.png)
+ 
+##Examples
+I've included an HTML file in the ```./tests/``` folder if you want to see an example of each of three functions.
+
+##Credits
+ - Firstly, and most importantly, credit is due to the incredible folks over at [Bootbox](http://bootboxjs.com/), including [Nick Payne](http://twitter.com/makeusabrew). Their stuff is awesome: I'm just piggybacking off of it.
+ - Secondly, I owe a lot of the styling to bootstrap. So yeh, those dudes are awesome too.
+ - Finally, the coding for Sandal Chest itself was done by me, [David Newman](https://github.com/Newms34/)
+
+##License
+Since I did not develop either Bootstrap or Bootbox, I have no control over those licenses. As far as Sandal Chest, however, you're free to redistribute/use it however you like. 
---
+++ js/main.js
@@ -0,0 +1,263 @@
+var sandalchest = {};
+var myCb = function(inp) {
+    $('#outptTest').val(inp);
+}
+sandalchest.drawDiv = function(opts, type, cb) {
+    var theSpeed = opts.options && opts.options.speed || 1000;
+    var modlBg = document.createElement('div');
+    modlBg.id = 'sandalchest-modal-bg';
+    modlBg.innerHTML = '&nbsp;';
+    var modalBox = document.createElement('div');
+    modalBox.className = 'col-sm-12 col-md-4 col-md-offset-4 sandalchest-modal-main';
+    modalBox.innerHTML = '<h2>' + (opts.title || 'Untitled') + '</h2>' + '<div class="sandalchest-text-main">' + (opts.text || 'No content!') + '</div>';
+
+    //modal finished! Append!
+    $(modlBg).append(modalBox)
+        //now add the special effects (scroll ends). top first
+    var modalBoxScTop = document.createElement('div');
+    var modalBoxScBot = document.createElement('div');
+    modalBoxScTop.className = 'sandalchest-modal-scroll-top';
+    modalBoxScBot.className = 'sandalchest-modal-scroll-bot';
+    $(modalBox).append(modalBoxScTop);
+    $(modalBox).append(modalBoxScBot);
+    //right scroll caps
+    var modalBoxScRT = document.createElement('div');
+    modalBoxScRT.className = 'sandalchest-modal-scroll-right';
+    $(modalBoxScTop).append(modalBoxScRT);
+    var modalBoxScRB = document.createElement('div');
+    modalBoxScRB.className = 'sandalchest-modal-scroll-right';
+    $(modalBoxScBot).append(modalBoxScRB);
+
+    //left scroll caps
+    var modalBoxScLT = document.createElement('div');
+    modalBoxScLT.className = 'sandalchest-modal-scroll-left';
+    $(modalBoxScTop).append(modalBoxScLT);
+    var modalBoxScLB = document.createElement('div');
+    modalBoxScLB.className = 'sandalchest-modal-scroll-left';
+    $(modalBoxScBot).append(modalBoxScLB);
+
+    $('body').append(modlBg);
+    $(modlBg).fadeIn(250);
+    $(modalBox).animate({
+        height: '80vh',
+        top: '10vh'
+    }, {
+        duration: theSpeed,
+        queue: false
+    });
+    $(modalBoxScTop).animate({
+        height: '0%'
+    }, {
+        duration: theSpeed,
+        queue: false
+    });
+    $(modalBoxScBot).animate({
+        height: '0%'
+    }, {
+        duration: theSpeed,
+        queue: false
+    });
+    $('.sandalchest-modal-scroll-right').animate({
+        width: '.25%'
+    }, {
+        duration: theSpeed,
+        queue: false
+    });
+    $('.sandalchest-modal-scroll-left').animate({
+        width: '.25%'
+    }, {
+        duration: theSpeed,
+        queue: false
+    });
+    //now add appropriate buttons
+    var okBut = document.createElement('button');
+    var noBut = document.createElement('button');
+    var inpBox = document.createElement('input');
+    okBut.className = 'btn btn-stone two-but';
+    noBut.className = 'btn btn-stone two-but-two';
+    okBut.id = 'sandalchest-okay';
+    noBut.id = 'sandalchest-nope';
+    okBut.tabIndex = 0;
+    noBut.tabIndex = 1;
+    okBut.innerHTML = 'Okay';
+    noBut.innerHTML = 'Cancel';
+    inpBox.id = 'sandalchest-input';
+    var promptVal = null;
+
+    if (type == 0) {
+        //alert
+        $(modalBox).append(okBut);
+        okBut.className = 'btn btn-stone one-but';
+    } else {
+        $(modalBox).append(okBut);
+        $(modalBox).append(noBut);
+        if (type == 1) {
+            //confirm
+        } else {
+            $('.sandalchest-text-main').append('<br/><br/>').append(inpBox);
+            //prompt
+            inpBox.tabIndex = 0;
+            okBut.tabIndex = 1;
+            noBut.tabIndex = 2;
+        }
+    }
+    document.querySelector('#sandalchest-okay').onkeyup = function(e) {
+        if (e.which == 27) {
+            $('#sandalchest-nope').click();
+        }
+    }
+    if (type < 2) {
+        document.querySelector('#sandalchest-okay').focus();
+    } else {
+        document.querySelector('#sandalchest-input').focus();
+        document.querySelector('#sandalchest-input').onkeyup = function(e) {
+            console.log(e)
+            if (e.which == 27) {
+                $('#sandalchest-nope').click();
+            } else if (e.which == 13) {
+                $('#sandalchest-okay').click();
+            }
+        }
+    }
+    okBut.onclick = function() {
+        $(modlBg).fadeOut(250, function() {
+            if (type > 1) {
+                promptVal = $('#sandalchest-input').val() || false;
+            } else if (type == 1) {
+                promptVal = true;
+            }
+            if (cb) {
+                cb(promptVal);
+            }
+            $(this).remove();
+        });
+    }
+    noBut.onclick = function() {
+        $(modlBg).fadeOut(250, function() {
+            if (cb) {
+                cb(null);
+            }
+            console.log(null)
+            $(this).remove();
+        });
+    }
+}
+sandalchest.alert = function(title, text, op, cb) {
+    //dealing with multiple param combos:
+    console.log(arguments);
+    var realTitle =null;
+    var realText = null;
+    var realOp = null;
+    var realCb = null;
+    for (var i=0;i<arguments.length;i++){
+      if(typeof arguments[i]=='string' && realTitle==null){
+        realTitle = arguments[i];
+      }else if (typeof arguments[i]=='string' && realText ==null){
+        realText = arguments[i];
+      }else if(typeof arguments[i]=='string'){
+        throw new Error('Too many string params supplied!');
+      }
+      if (typeof arguments[i]=='object' && realOp == null){
+        realOp = arguments[i];
+      }else if (typeof arguments[i]=='object'){
+        throw new Error('Too many configuration objects supplied!')
+      }
+      if (typeof arguments[i]=='function' && realCb==null){
+        realCb = arguments[i];
+      }else if (typeof arguments[i]=='function'){
+        throw new Error('You can only have one callback function!')
+      }
+    }
+    var opts = {
+      title:realTitle,
+      text:realText,
+      cb:realCb,
+      options:realOp
+    }
+    sandalchest.drawDiv(opts, 0, cb);
+}
+sandalchest.confirm = function(title, text, op, cb) {
+    //dealing with multiple param combos:
+    console.log(arguments);
+    var realTitle =null;
+    var realText = null;
+    var realOp = null;
+    var realCb = null;
+    for (var i=0;i<arguments.length;i++){
+      if(typeof arguments[i]=='string' && realTitle==null){
+        realTitle = arguments[i];
+      }else if (typeof arguments[i]=='string' && realText ==null){
+        realText = arguments[i];
+      }else if(typeof arguments[i]=='string'){
+        throw new Error('Too many string params supplied!');
+      }
+      if (typeof arguments[i]=='object' && realOp == null){
+        realOp = arguments[i];
+      }else if (typeof arguments[i]=='object'){
+        throw new Error('Too many configuration objects supplied!')
+      }
+      if (typeof arguments[i]=='function' && realCb==null){
+        realCb = arguments[i];
+      }else if (typeof arguments[i]=='function'){
+        throw new Error('You can only have one callback function!')
+      }
+    }
+    var opts = {
+      title:realTitle,
+      text:realText,
+      cb:realCb,
+      options:realOp
+    }
+    sandalchest.drawDiv(opts, 1, cb);
+}
+sandalchest.prompt = function(title, text, op, cb) {
+    //dealing with multiple param combos:
+    console.log(arguments);
+    var realTitle =null;
+    var realText = null;
+    var realOp = null;
+    var realCb = null;
+    for (var i=0;i<arguments.length;i++){
+      if(typeof arguments[i]=='string' && realTitle==null){
+        realTitle = arguments[i];
+      }else if (typeof arguments[i]=='string' && realText ==null){
+        realText = arguments[i];
+      }else if(typeof arguments[i]=='string'){
+        throw new Error('Too many string params supplied!');
+      }
+      if (typeof arguments[i]=='object' && realOp == null){
+        realOp = arguments[i];
+      }else if (typeof arguments[i]=='object'){
+        throw new Error('Too many configuration objects supplied!')
+      }
+      if (typeof arguments[i]=='function' && realCb==null){
+        realCb = arguments[i];
+      }else if (typeof arguments[i]=='function'){
+        throw new Error('You can only have one callback function!')
+      }
+    }
+    var opts = {
+      title:realTitle,
+      text:realText,
+      cb:realCb,
+      options:realOp
+    }
+    sandalchest.drawDiv(opts, 2, cb);
+}
+
+//TEST FNS
+
+document.querySelector('#doAlrt').onclick = function() {
+    sandalchest.alert('Alerts Are Cool!', 'All the cool kids are using alerts!', myCb);
+}
+document.querySelector('#doSlowAlrt').onclick = function() {
+    sandalchest.alert({speed:4000},'Alerts Are Cool!', 'All the cool kids are using alerts!', myCb);
+}
+
+document.querySelector('#doConf').onclick = function() {
+    sandalchest.confirm('Whoa!', 'Are you sure you wanna use SandalChest?', myCb);
+}
+
+document.querySelector('#doProm').onclick = function() {
+    sandalchest.prompt('Prompts too!', 'No more ugly vanilla window.prompts!', myCb);
+}
